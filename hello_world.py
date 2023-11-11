9ab02230329cb496c462369e48568a17f140d935
Kristin Maxwell <kristinmaxwell43@gmail.com>
2023-11-11 14:23:50 -0700

    hello_world and test to commit

---
+++ hello_world.py
@@ -0,0 +1,8 @@
+print("Hello Python World!")
+print("I finally got vs code connected to github!")
+message = "Now we working on variables!"
+print(message)
+message = "python crash course reader!"
+print(message)
+variable = "variables are labels that you assign to values."
+print(variable)
